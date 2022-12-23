64808a34f1910b7b563e54bbaf08268306cba510
wilson cutipa <wilson_dani@hotmail.com>
2022-12-22 19:43:13 -0500

    api

--- semana_8/3-chanchirata/js/modules/foods.js
+++ semana_8/3-chanchirata/js/modules/foods.js
@@ -1,30 +1,40 @@
-import { comidas } from "../../util/data.js";
-
 const foods = () => {
-  const foodsFoods = document.getElementById('foodsFoods');
-  comidas.forEach((element) => {
-    const { id, name, description, urlImage, urlRecipe } = element;
+  const renderFoods = (data) => {
+    const foodsFoods = document.getElementById('foodsFoods');
 
-    foodsFoods.innerHTML += `
-      <div class="col-md-4" key=${id}>
-        <div class="card bg-dark border border-2 border-light border-opacity-25 mx-auto"
-          style="width: min(100%, 18rem);">
-          <div class="card-header">
-            ${name}
-          </div>
-          <div class="card-body">
-            <img src="${urlImage}" class="card-img-top" alt="Anime 1" width="256" height="144"
-              style="width: 100%; height: 192px; object-fit: cover; border-radius: 0.5rem">
-            <h5 class="card-title mt-2">${name}</h5>
-            <p class="card-text">${description}</p>
-          </div>
-          <div class="card-footer text-muted">
-            <a href="${urlRecipe}" class="btn btn-primary">Receta</a>
+    data.forEach((element) => {
+      //const { id, name, description, urlImage, urlRecipe } = element;
+      const { id, title, price, description, category, image, rating } = element;
+
+      foodsFoods.innerHTML += `
+        <div class="col-md-4" key=${id}>
+          <div class="card bg-dark border border-2 border-light border-opacity-25 h-100 mx-auto"
+            style="width: min(100%, 18rem);">
+            <div class="card-header text-center">
+              ${title}
+            </div>
+            <div class="card-body">
+              <img src="${image}" class="card-img-top" alt="Anime 1" width="256" height="144"
+                style="width: 100%; height: 192px; object-fit: cover; border-radius: 0.5rem">
+              <h5 class="card-title mt-2">${category}</h5>
+              <p class="card-text">${description}</p>
+              <p class="card-text">${price}</p>
+            </div>
+            <div class="card-footer text-center">
+              <a href="${rating}" target="_blank" rel="noopener noreferrer" class="btn btn-primary">Receta</a>
+            </div>
           </div>
         </div>
-      </div>
-    `;
-  });
+      `;
+    });
+  };
+
+  const fetchRead = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    renderFoods(data);
+  };
+  fetchRead();
 };
 
 export default foods;
