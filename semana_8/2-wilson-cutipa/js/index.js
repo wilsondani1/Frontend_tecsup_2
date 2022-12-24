a658a3d88fd4a3a1aecef9171b9fb5106e51607f
wilson cutipa <wilson_dani@hotmail.com>
2022-12-23 23:07:45 -0500

    s8.

--- semana_8/2-wilson-cutipa/js/index.js
+++ semana_8/2-wilson-cutipa/js/index.js
@@ -2,11 +2,9 @@
 'use strict';
 
 import header from "./modules/header.js";
-import characters from "./modules/characters.js";
 
 const documentReady = () => {
   header();
-  characters();
 };
 
 document.addEventListener('DOMContentLoaded', documentReady);
---
+++ semana_8/4-rick-and-morty/js/modules/header.js
@@ -0,0 +1,16 @@
+import { fetchReadCharacters, renderCharacters } from "../utils/utils.js";
+
+const header = () => {
+  const searchForm = document.getElementById('searchForm');
+
+  const searchCharacters = async (e) => {
+    e.preventDefault();
+    const name = searchForm.name.value;
+    const data = await fetchReadCharacters(1, name);
+    renderCharacters(data);
+  };
+
+  searchForm.addEventListener('submit', searchCharacters);
+};
+
+export default header;
