731219754fe3c16063dd42c5a89778a723fd1509
wilson cutipa <wilson_dani@hotmail.com>
2023-01-05 22:48:16 -0500

    react

---
+++ semana_9/1-patita-feliz/src/componentes/secions/header.jsx
@@ -0,0 +1,13 @@
+const Header = ({ company }) => {
+    const { name, slogan } = company;
+    return (
+      <header className="py-3">
+        <div className="container d-flex flex-column gap-1">
+          <h2 className="text-info text-center fs-2 m-0">{name}</h2>
+          <h1 className="text-primary text-center fs-5 m-0">{slogan}</h1>
+        </div>
+      </header>
+    );
+  };
+  
+  export default Header;
