bafd69989b198436d24c4e2fb4e74829f353f8e5
wilson cutipa <wilson_dani@hotmail.com>
2023-01-11 17:27:44 -0500

    s_9 end class 1

--- semana_9/1-patita-feliz/src/components/Appointments/AppointmentsForm.jsx
+++ semana_9/1-patita-feliz/src/components/Appointments/AppointmentsForm.jsx
@@ -1,14 +1,38 @@
-const  AppointmentsForm= () => {
+import { useState } from "react";
+
+const  AppointmentsForm = () => {
+   
+        const [AppointmentsForm, setAppointmentsForm] = useState({
+            
+            nombre:'',
+            mascota:'',
+            propietario:'',
+            fecha: '',
+            hora:'',
+            sintomas:'',
+  
+        });
+        const handleInput = (e) => {setAppointmentsForm({
+            ...AppointmentsForm,
+            [e.target.id]:e.target.value
+        } )
+        };
+    
+
     return (
         <div>
             <h3 className="text-center m-1">🚑Crear cita🚑</h3>
             <form className="text-dark">
+            
+        
             <div class="form-floating mb-3">
+
                 <input 
                 type="text" 
                 class="form-control" 
                 id="mascota" 
                 placeholder="Nombre de la mascota"
+                onInput={handleInput}
                 />
                 <label for="mascota">Nombre de la mascota </label>
             </div>
@@ -19,6 +43,7 @@ const  AppointmentsForm= () => {
                 class="form-control" 
                 id="propietario" 
                 placeholder="Nombre de la propietario"
+                onInput={handleInput}
                 />
                 <label for="propietario">Nombre del propietario </label>
             </div>
@@ -29,18 +54,20 @@ const  AppointmentsForm= () => {
                 class="form-control" 
                 id="fecha" 
                 placeholder=" fecha"
+                onInput={handleInput}
                 />
                 <label for="fecha">fecha </label>
             </div>
 
             <div class="form-floating mb-3">
                 <input 
-                type="text" 
+                type="time" 
                 class="form-control" 
-                id="Hora" 
-                placeholder=" Hora"
+                id="hora" 
+                placeholder=" hora"
+                onInput={handleInput}
                 />
-                <label for="Hora">Hora </label>
+                <label for="hora">Hora </label>
             </div>
 
             <div class="form-floating mb-3">
@@ -49,11 +76,23 @@ const  AppointmentsForm= () => {
                 class="form-control" 
                 id="sintomas" 
                 placeholder="sintomas"
+                onInput={handleInput}
                 />
                 <label for="sintomas">sintomas </label>
             </div>
+
             <button className="btn btn-info w-100 fw-bold"> Crear</button>
+
             </form>
+            <ul>
+                <li>{AppointmentsForm.nombre}</li>
+                <li>{AppointmentsForm.mascota}</li>
+                <li>{AppointmentsForm.propietario}</li>
+                <li>{AppointmentsForm.fecha}</li>
+                <li>{AppointmentsForm.hora}</li>
+                <li>{AppointmentsForm.sintomas}</li>
+
+            </ul>
         </div>
    
      );
