d0609118610f4830b4873a373054b430a912d5ba
wilson cutipa <wilson_dani@hotmail.com>
2023-01-13 23:10:16 -0500

    s10_3

--- semana_10/vite-project/src/components/sections/Header.jsx
+++ semana_10/vite-project/src/components/sections/Header.jsx
@@ -1,27 +1,84 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import logoEAKON from '../../assets/img/wilson_logo.jpg';
 
 const Header = () => {
+
+  const header = useRef();
+  const modal = useRef();
+
+  const showMenu = () => {
+    header.current.classList.add('header--menu');
+    modal.current.classList.add('modal--show');
+  };
+
+  const hideMenu = () => {
+    header.current.classList.remove('header--menu');
+    modal.current.classList.remove('modal--show');
+  };
+
   return (
-    <header className="header">
+    <header
+      className="header"
+      ref={header}
+    >
       <nav className="nav">
         <div className="container f-elements f-elements--header">
           <Link to="/">
             <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
           </Link>
-          <div className="modal modal--header">
-            <ul className="list list--header">
+
+          <div    className="modal modal--header"
+            ref={modal}
+            onClick={hideMenu}>
+
+
+            
+            <ul className="list list--header"
+              onClick={e=>e.stopPropagation()}>
+
               <li>
-                <Link to="/productos" className="list__link">Productos</Link>
+                <Link 
+                to="/productos"
+                 className="list__link"
+                 onClick={hideMenu}
+                 >
+                Productos
+                 
+                 </Link>
               </li>
               <li>
-                <Link to="/servicios" className="list__link">Servicios</Link>
+                <Link 
+                to="/servicios" 
+                className="list__link"
+                onClick={hideMenu}
+                >
+                Servicios
+                
+                </Link>
               </li>
               <li>
-                <Link to="/nosotros" className="list__link">Nosotros</Link>
+                <Link 
+                to="/nosotros" 
+                className="list__link"
+                onClick={hideMenu}
+                >
+                Nosotros
+                
+                </Link>
               </li>
               <li>
-                <Link to="/contacto" className="list__link">Contacto</Link>
+                <Link 
+                to="/contacto" 
+                className="list__link"
+                onClick={hideMenu}
+                >
+                Contacto
+                
+                </Link>
+              </li>
+              <li className='icon icon-close' onClick={hideMenu}>
+                ❌
               </li>
             </ul>
           </div>
@@ -31,7 +88,9 @@ const Header = () => {
             <option value="value1">☀</option>
             <option value="value1">💻</option>
           </select>
-          <button className='icon'> 
+          <button cclassName="icon"
+              onClick={showMenu}
+              > 
           🍔
           </button>
           </div>
