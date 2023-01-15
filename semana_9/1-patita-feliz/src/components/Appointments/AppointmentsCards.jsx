ffa720ae341fb281c5155a2edd09183b52d4eae9
wilson cutipa <wilson_dani@hotmail.com>
2023-01-15 18:51:27 -0500

    cuadro

--- semana_9/1-patita-feliz/src/components/Appointments/AppointmentsCards.jsx
+++ semana_9/1-patita-feliz/src/components/Appointments/AppointmentsCards.jsx
@@ -1,28 +1,26 @@
 const AppointmentsCards = ({ appointments }) => {
     return (
         <div className="d-flex flex-column gap-3">
-        <h3 className  ="text-center m-1">🚑Cita🚑</h3>
+        <h3 className  ="text-center m-0">🚑Cita🚑</h3>
         {appointments.map((element,index)=>{
             const { mascota ,propietario,fecha,hora,sintomas} = element;
             return(
         <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+                <a href="#" className="list-group-item list-group-item-action " aria-current="true">
                 <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">Mascota:{mascota}</h5>
-                    <small>3 days ago</small>
+                    <small>{fecha}{hora}</small>
                 </div>
-                <p className="mb-1">Some placeholder content in a paragraph.</p>
-                <small>And some small print.</small>
+
+                <p className="mb-1">{sintomas}</p>
+                <p className="mb-1">{propietario}</p>
+                <small>{propietario}</small>
                 </a>
         </div>
             );               
         })}
   
-
-        </div>
-
-
-
+      </div>
     );
 };
 
